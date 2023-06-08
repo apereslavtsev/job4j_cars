a8ec76fa814b593a29f3c3c87e2a5ac6efbf09e5
apereslavtsev <pereslavtsev.anton@gmail.com>
2023-06-08 09:51:13 +0300

    3. Доменная модель проекта "АвтоМаг" [#504897 # [#504897]]

---
+++ db/scripts/001_ddl_create_initial_schema.sql
@@ -0,0 +1,13 @@
+create table auto_user (
+    id        serial primary key,
+    login     varchar unique not null,
+    password  varchar        not null
+    );
+
+
+create table auto_post (
+    id serial primary key,
+    description  varchar,
+    created timestamp not null,
+    auto_user_id    int references auto_user (id) not null
+    );
